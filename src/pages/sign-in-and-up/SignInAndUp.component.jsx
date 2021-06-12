468f222990cac1a00e95ed683727f9d5b982b403
A. Aladdin <66479204+aladdin4@users.noreply.github.com>
2021-06-13 00:31:47 +0200

    added styles to the sign-in/up page

--- src/pages/sign-in-and-up/SignInAndUp.component.jsx
+++ src/pages/sign-in-and-up/SignInAndUp.component.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import SignIn from "../../components/sign-in/signIn.component";
 import SignUp from "../../components/sign-up/signUp.component";
 
+import "./SignInAndUp.styles.scss";
 const SignInUpPage = (prop) => {
   return (
-    <div>
+    <div className="sign-in-and-sign-up">
       <SignIn />
       <SignUp />
     </div>
