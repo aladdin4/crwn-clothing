7c1c63ef1a45a59d876b2a0911b8ea5ed4671c16
A. Aladdin <66479204+aladdin4@users.noreply.github.com>
2021-06-16 15:21:07 +0200

    created the user action to handle log in and out

---
+++ src/redux/user/user.actions.js
@@ -0,0 +1,8 @@
+// action creator: called by the evenet-handlers, sent a raw event object and supposed to return a consumable object to be used by the reducer
+
+export const setCurrentUser = (user) => {
+  return {
+    type: "SET_CURRENT_USER",
+    payload: user,
+  };
+};
