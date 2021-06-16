f3ca9f71861c111d70980190350dc33c0f8d58a9
A. Aladdin <66479204+aladdin4@users.noreply.github.com>
2021-06-16 14:36:34 +0200

    installed and set-up redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+
+import CombineReducers from "./redux/root-reducer";
+
+const store = createStore(CombineReducers);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* provider provides the store to the APP */}
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
