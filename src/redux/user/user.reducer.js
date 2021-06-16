c3eee7ec982ec188b8e94f241ea74b3c97c5583b
A. Aladdin <66479204+aladdin4@users.noreply.github.com>
2021-06-16 14:36:23 +0200

    created the first reducer (userReducer)

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,16 @@
+// reducer is a function that takes the currenrt state + action object and returns the new state
+
+const INIT_STATE = null;
+
+// state should be default out, because if undef. value will return exception
+// action is an object that comes from action-creators, it contais 2 properties
+// // A- action.type    B-action.payload
+export default userReducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      return { ...state, currentUser: action.payload };
+
+    default:
+      return state;
+  }
+};
