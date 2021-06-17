1a283664968c61e29c450f2152f0cbf320980e42
A. Aladdin <66479204+aladdin4@users.noreply.github.com>
2021-06-17 15:15:36 +0200

    updated the action creator and reducer

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
-// action creator: called by the evenet-handlers, sent a raw event object and supposed to return a consumable object to be used by the reducer
+// action creator: called by the event-handlers, sent a raw event object and supposed to return a consumable object to be used by the reducer
 
 export const setCurrentUser = (user) => {
   return {
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,11 +1,11 @@
 // reducer is a function that takes the currenrt state + action object and returns the new state
 
-const INIT_STATE = null;
+const INIT_STATE = { currentUser: "null" };
 
 // state should be default out, because if undef. value will return exception
 // action is an object that comes from action-creators, it contais 2 properties
 // // A- action.type    B-action.payload
-export default userReducer = (state = INIT_STATE, action) => {
+export const userReducer = (state = INIT_STATE, action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
       return { ...state, currentUser: action.payload };
