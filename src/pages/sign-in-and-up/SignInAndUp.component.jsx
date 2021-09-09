ca15e73742a46b7eb6b2947060ff8733713560fd
A. Aladdin <66479204+aladdin4@users.noreply.github.com>
2021-09-09 18:25:21 +0200

    Update SignInAndUp.component.jsx

--- src/pages/sign-in-and-up/SignInAndUp.component.jsx
+++ src/pages/sign-in-and-up/SignInAndUp.component.jsx
@@ -3,7 +3,7 @@ import SignIn from "../../components/sign-in/signIn.component";
 import SignUp from "../../components/sign-up/signUp.component";
 
 import "./SignInAndUp.styles.scss";
-const SignInUpPage = (prop) => {
+const SignInUpPage = (props) => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
