363d615ea3532ce3ecb1187a23483a39e5b043d4
A. Aladdin <66479204+aladdin4@users.noreply.github.com>
2021-09-09 18:24:33 +0200

    Update root-reducer.js

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,8 +5,10 @@ import { combineReducers } from "redux";
 import persistReducer from "redux-persist/es/persistReducer";
 import storage from "redux-persist/lib/storage";
 
-import cartReducer from "./cart/cart.reducer";
 import { userReducer } from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 // config. obj. for our persist-reducer
 const persistConfig = {
@@ -18,8 +20,9 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 
 // export default rootReducer;
-
 export const persistedReducer = persistReducer(persistConfig, rootReducer);
